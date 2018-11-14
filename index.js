c94430deed2712ddcc3a929f09ad166c481ed8ed
Cooldude2606 <cooldude260607@gmail.com>
2018-11-14 20:45:13 +0000

    Bug in index.js

--- index.js
+++ index.js
@@ -14,16 +14,17 @@ const modules = {
 
 console.log('Loading Web Modules...')
 for (let module_name in webModules) {
-    console.log(`Starting Module: ${module_name}`)
-    App.use('/'+module_name,require(modules[module_name]))
+    console.log(` Starting Module: ${module_name}`)
+    App.use('/'+module_name,require(webModules[module_name]))
 }
 
 console.log('Loading Other Modules...')
-for (let module_name in webModules) {
-    console.log(`Starting Module: ${module_name}`)
+for (let module_name in modules) {
+    console.log(` Starting Module: ${module_name}`)
     require(modules[module_name])
 }
 
+console.log('Starting Web Server')
 App.listen(process.env.HOST_PORT,process.env.HOST_IP,() => {
-    console.log(`Listening on: ${process.env.HOST_IP}:${process.env.HOST_PORT}`)
+    console.log(` Listening on: ${process.env.HOST_IP}:${process.env.HOST_PORT}`)
 })
